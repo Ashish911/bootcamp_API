9c2fe8a1df5e5f21a4b8673214551249a19fa789
Ashish <ashishDongol32@gmail.com>
2021-03-24 08:30:36 +0545

    implementing of advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,13 +7,20 @@ const Bootcamp = require('../models/Bootcamp');
 // Get all bootcamps (GET METHOD) (/api/v1/bootcamps)
 // Public route
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    
-    const bootcamp = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
-        data: bootcamp
+        data: bootcamps
     })
 });
 
